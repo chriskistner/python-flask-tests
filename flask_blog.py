4b41fe7758dd027204946517eb56bdef9fbaef57
Christopher Kistner <chris.kistner0@gmail.com>
2019-01-23 11:58:48 -0800

    Basic setup of flask framework with blocks and routes completed

---
+++ flask_blog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+books = [
+    {
+        "author": "Chris Kistner",
+        "title": "The Pangaea Project: Homecoming",
+        "description": "The epic adventure of the Space Colony Pangaea as it returns to Earth.",
+        "date_published": '10/15/2012'
+    },
+        {
+        "Author": "Chris Kistner",
+        "title": "The Pangaea Project: Revenants",
+        "description": "The Adventure Continues as Colonel West leads an infiltration team to Earth.",
+        "date_published": '4/8/2015'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', books=books)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug = True)
